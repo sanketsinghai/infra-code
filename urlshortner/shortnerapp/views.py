486661abff851c66a38f208c6daffe6b589df111
sanket singhai <sanketsinghai12@gmail.com>
2021-06-02 00:29:02 +0530

    add url shortner code

--- urlshortner/shortnerapp/views.py
+++ urlshortner/shortnerapp/views.py
@@ -1,3 +1,40 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import pyshorteners
+import json
+import os
 # Create your views here.
+
+@api_view(["POST"])
+def home(request):
+    if request.method == "POST":
+        data = request.data
+        link = data["url"]
+        module_dir = os.path.dirname(__file__)  
+        file_path = os.path.join(module_dir, "urlmap.json")
+        with open(file_path, "r+") as f:
+            d = json.load(f)
+            cache_key = d.get(link)
+            if cache_key:
+                return Response(
+                    {
+                        "status": 200,
+                        "shorturl": cache_key,
+                        "message": "successfully short the url",
+                    }
+                )
+            else:
+                shortners = pyshorteners.Shortener()
+                newData = shortners.tinyurl.short(link)
+                d[link] = newData
+                f.seek(0)
+                json.dump(d, f)
+                return Response(
+                    {
+                        "status": 200,
+                        "shorturl": newData,
+                        "message": "successfully short the url",
+                    }
+                )
